7b820bab69550a748a839591b5899e75a0048616
bain-liao <liao.bain@mths.ca>
2024-09-17 11:25:33 -0400

    Changed comment and added math

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bain Liao
+ * Created on: Sep 2024
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+//multiply
+basic.showString('A rectangle has dimensions 5 cm & 3 cm. The perimeter would be:' + (2 * (5 + 3)).toString()),basic.showString('cm.')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
+
+//multiply
+basic.showString('The area would be:' + (5 * 3).toString()),basic.showString('cm^2.')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
