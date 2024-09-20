d61b69afd3f452a6b1146523491e795c2f76b3c3
bain-liao <liao.bain@mths.ca>
2024-09-20 15:15:25 +0000

    made math code in python and hexed it

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,28 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Bain Liao
+Created on: Sep 2024
+This module does basic math
 """
 
 from microbit import *
+from time import sleep
 
+display.clear()
+sleep(1)
 
-display.scroll("Hello, World!")
+# Perimeter
+display.scroll(
+    "A rectangle has dimensions 5 cm & 3 cm. The perimeter would be: "
+    + str(2 * (5 + 3)),
+    display.scroll("cm"),
+)
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
+
+# Area
+display.scroll("The area would be: " + str(5 * 3))
+display.scroll("cm^2")
+display.show(Image.HAPPY)
+sleep(0.5)
+display.clear()
